56d89e9a7beb7c6afe41fc93d08012a9e758fd2d
cpashp <cpelus1@students.towson.edu>
2017-11-14 20:53:02 -0500

    Each class is fully commented for ease of readability

--- src/main/scala/VarDefNode.scala
+++ src/main/scala/VarDefNode.scala
@@ -1,5 +1,8 @@
 class VarDefNode(name: String, value: String) {
-
+/*
+constructs variable object based on name and value
+method returns the string value the object holds
+ */
   def valueOut() ={
     return value
   }
